8d0be832d4278b3ffd32af6d4319db4f5c8f00e5
Jonas Merkle [JJM] <jonas_merkle@ymail.com>
2019-04-21 23:44:37 +0200

    lib compile test

--- firmware/universal-i2c-sensor-master-unit-firmware/src/main.cpp
+++ firmware/universal-i2c-sensor-master-unit-firmware/src/main.cpp
@@ -1,4 +1,20 @@
+// system libraries
 #include <Arduino.h>
+
+// sensor libraries
+#include <Adafruit_BNO055.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_VL53L0X.h>
+#include <Adafruit_VL6180X.h>
+#include <SRF08.h>
+
+// i2c mux library
+#include <TCA9548A.h>
+
+// communication libraries
+#include <Wire.h>
+
+
 
 void setup() {
   // put your setup code here, to run once:
