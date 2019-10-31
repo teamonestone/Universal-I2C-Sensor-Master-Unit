5356ce9ed38e42be53a17ae5ec63d210f5038aa8
Jonas Merkle [JJM] <jonas@jjm.one>
2019-10-31 22:17:22 +0100

    Doku zur OME

--- firmware/universal-i2c-sensor-master-unit-firmware/include/ObjectManagementEngine.h
+++ firmware/universal-i2c-sensor-master-unit-firmware/include/ObjectManagementEngine.h
@@ -1,54 +1,57 @@
-#ifndef ObjectManagementEngine_h
-#define ObjectManagementEngine_h
+/**
+ * @file ObjectManagementEngine.h
+ * @brief The header file of the Object Management Engine class.
+ * @author Jonas Merkle [JJM] <a href="mailto:jonas.merkle@tam-onestone.net">jonas.merkle@tam-onestone.net</a>
+ * @author Dominik Authaler <a href="mailto:dominik.authaler@team-onestone.net">dominik.authaler@team-onestone.net</a>
+ * @author
+ * This library is maintained by <a href="https://team-onestone.net">Team Onestone</a>.
+ * E-Mail: <a href="mailto:info@team-onestone.net">info@team-onestone.net</a>
+ * @version 1.0.0
+ * @date 31 October 2019
+ * @copyright This project is released under the GNU General Public License v3.0
+ */
 
-//////////////
-// Includes //
-//////////////
+#ifndef OBJECTMANAGEMENTENGINE_h
+#define OBJECTMANAGEMENTENGINE_h
+
+// Includes
 #include <inttypes.h>
 
-/////////////
-// Defines //
-/////////////
+// Defines
+#define OME_OBJ_SIZE 64			///< Define for object size (in bytes).
+#define OME_OBJ_POOL_SIZE 8		///< Definie for pool size.
 
-// define for object size (in bytes)
-#define OME_OBJ_SIZE 64
-
-// definie for pool size 
-#define OME_OBJ_POOL_SIZE 8
-
-///////////////
-// OME-Class //
-///////////////
-
+/**
+ * @class OME
+ * 
+ * @brief This calls manages a given amout of memory that then can be used for other objects.
+ */
 class OME
 {
 // Begin PUBLIC ------------------------------------------------------------------
 	public:
         
-        // get a free ptr
-        static void* GetFreeObjPrt(int16_t *poolNo);
-        
-        // free a used ptr
-        static bool FreePtr(void *objPrt);
-        static bool FreePtr(int16_t poolNo);
+        // functions
+        static void* GetFreeObjPrt(int16_t *poolNo);		// Get a a pointer to the next free availiale memory slice.
+        static bool FreePtr(void *objPrt);                	// Mark a blocked memory slice as free based on a given pointer.
+        static bool FreePtr(int16_t poolNo);				// Mark a blocked memory slice as free based on the pointer number.
 
 // End PUBLIC --------------------------------------------------------------------
 
 // Begin PRIVATE -----------------------------------------------------------------
 	private:
         
-        // reserved memeory to store the objects in
-        static uint8_t BytePool[OME_OBJ_POOL_SIZE * OME_OBJ_SIZE];
+        // constructor
+        OME();			// Main constructor of the class (private so no instance of this class can be created).
+        ~OME();			// Main destructor of the class (private so no instance of this class can be created).
+		
+		// vars & objects
+        static uint8_t BytePool[OME_OBJ_POOL_SIZE * OME_OBJ_SIZE];		// The memory witch gets split into smaller memory slices.
+        static bool inUse[OME_OBJ_POOL_SIZE];							// The array with usage inforation.
 
-        // managment stuff
-        static bool inUse[OME_OBJ_POOL_SIZE];
-
-        // constructor -> no instance can be created
-        OME();
-        ~OME();
-
-        static void* ConvertNoToPtr(int16_t poolNo);
-        static int16_t ConvertPrtToNo(void *objPtr);
+        // functions
+        static void* ConvertNoToPtr(int16_t poolNo);					// Convert a pointer number in an actual pointer.
+        static int16_t ConvertPrtToNo(void *objPtr);					// Convert an actual pointer in a pointer number.
 
 // End PRIVATE -------------------------------------------------------------------
 
--- firmware/universal-i2c-sensor-master-unit-firmware/src/ObjectManagementEngine.cpp
+++ firmware/universal-i2c-sensor-master-unit-firmware/src/ObjectManagementEngine.cpp
@@ -1,10 +1,29 @@
-// associated header
+/**
+ * @file ObjectManagementEngine.h
+ * @brief The source file of the Object Management Engine class.
+ * @author Jonas Merkle [JJM] <a href="mailto:jonas.merkle@tam-onestone.net">jonas.merkle@tam-onestone.net</a>
+ * @author Dominik Authaler <a href="mailto:dominik.authaler@team-onestone.net">dominik.authaler@team-onestone.net</a>
+ * @author
+ * This library is maintained by <a href="https://team-onestone.net">Team Onestone</a>.
+ * E-Mail: <a href="mailto:info@team-onestone.net">info@team-onestone.net</a>
+ * @version 1.0.0
+ * @date 31 October 2019
+ * @copyright This project is released under the GNU General Public License v3.0
+ */
+
+// includes 
 #include <ObjectManagementEngine.h>
 
 //////////////////////
 // public functions //
 //////////////////////
 
+/**
+ * @brief Get a a pointer to the next free availiale memory slice.
+ * 
+ * @param poolNo int16_t pointer where the pointer number gets written to on succes, else -1.
+ * @return void pointer to the memory slice if there is one avaliable, else nullptr.
+ */
 void* OME::GetFreeObjPrt(int16_t *poolNo) {
     for (int16_t i = 0; i < OME_OBJ_POOL_SIZE; i++) {
         if (!inUse[i]) {
@@ -21,6 +40,12 @@ void* OME::GetFreeObjPrt(int16_t *poolNo) {
     return nullptr;
 }
 
+/**
+ * @brief Mark a blocked memory slice as free based on a given pointer.
+ * 
+ * @param objPrt The pointer to the object.
+ * @return true on succes, else false.
+ */
 bool OME::FreePtr(void *objPrt) {
     int16_t poolNo = ConvertPrtToNo(objPrt);
     if (poolNo >= OME_OBJ_POOL_SIZE || poolNo < 0) {
@@ -30,6 +55,12 @@ bool OME::FreePtr(void *objPrt) {
     return true;
 }
 
+/**
+ * @brief Mark a blocked memory slice as free based on the pointer number.
+ * 
+ * @param poolNo The number of the pointer.
+ * @return true on succes, else false.
+ */
 bool OME::FreePtr(int16_t poolNo) {
     if (poolNo >= OME_OBJ_POOL_SIZE || poolNo < 0) {
         return false;
@@ -42,12 +73,39 @@ bool OME::FreePtr(int16_t poolNo) {
 // private functions //
 ///////////////////////
 
+/**
+ * @brief Main constructor of the class (private so no instance of this class can be created).
+ * 
+ */
 OME::OME() {};
+
+/**
+ * @brief Main destructor of the class (private so no instance of this class can be created).
+ * 
+ */
 OME::~OME() {};
 
+
+/**
+ * @brief Initialize the memory witch gets split into smaller memory slices.
+ * 
+ */
 uint8_t BytePool[OME_OBJ_POOL_SIZE * OME_OBJ_SIZE] = {0};
+
+/**
+ * @brief Initialize the Array with usage inforation.
+ * 
+ * @param argname desc
+ * @return
+ */
 bool inUse[OME_OBJ_POOL_SIZE] = {0};
 
+/**
+ * @brief Convert a pointer number in an actual pointer.
+ * 
+ * @param poolNo The pointer number.
+ * @return The actual pointer as void*.
+ */
 void* OME::ConvertNoToPtr(int16_t poolNo) {
     if (poolNo >= OME_OBJ_POOL_SIZE || poolNo < 0) {
         return nullptr;
@@ -57,6 +115,12 @@ void* OME::ConvertNoToPtr(int16_t poolNo) {
     }
 }
 
+/**
+ * @brief Convert an actual pointer in a pointer number.
+ * 
+ * @param objPtr The actual pointer.
+ * @return The pointer number as int16_t.
+ */
 int16_t OME::ConvertPrtToNo(void *objPtr) {
     int16_t poolNo = -1;
     for (int16_t i = 0; i < OME_OBJ_POOL_SIZE; i++) {
